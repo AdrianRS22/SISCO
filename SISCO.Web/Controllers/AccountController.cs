7435bb746d1826d8749ce09897271ac43812abde
Adrian <adrianrojasanchez@gmail.com>
2021-04-02 21:52:01 -0600

    añadiendo el rol de usuario y modificaciones al login

--- SISCO.Web/Controllers/AccountController.cs
+++ SISCO.Web/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using SISCO.Web.Models;
@@ -155,8 +156,18 @@ namespace SISCO.Web.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    var roleName = "Usuario";
+
+                    if (!roleManager.RoleExists(roleName))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+
+                    await UserManager.AddToRoleAsync(user.Id, roleName);
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
