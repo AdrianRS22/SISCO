36f41c7fcc29d8c901386383526041b947eaf11a
eduartavia <eduartavia@gmail.com>
2021-04-22 17:51:35 -0600

    Producto

---
+++ SISCO.Web/Controllers/ProductoController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+using SISCO.CapaDatos.ViewModels;
+using SISCO.CapaLogica;
+
+
+namespace SISCO.Web.Controllers
+{
+    public class ProductoController : Controller
+    {
+
+        public ActionResult Lista()
+        {
+            var listaProductos = ProductoBLL.FetchAll();
+            return View(listaProductos);
+        }
+
+        public ActionResult Agregar()
+        {
+            var listaProveedor = ProveedorBLL.FetchAll();
+            ViewBag.data = listaProveedor;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(ProductoViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ProductoBLL.Add(modelo);
+                    return RedirectToAction("Lista");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Ocurrio un error al registrar");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Editar(Guid Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var producto = ProductoBLL.Fetch(Id);
+
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            var listaProveedor = ProveedorBLL.FetchAll();
+            ViewBag.data = listaProveedor;
+            return View(producto);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ProductoViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    ProductoBLL.Update(modelo);
+                    return RedirectToAction("Lista");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Ocurrio un error al editar");
+                }
+            }
+
+            return View(modelo);
+        }
+
+        public ActionResult Eliminar(Guid Id)
+        {
+            ProductoBLL.Eliminar(Id);
+            return RedirectToAction("Lista");
+        }
+       
+    }
+}
