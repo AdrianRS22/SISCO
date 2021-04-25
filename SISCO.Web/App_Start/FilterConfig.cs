422083c08b297ee040f33e3a853bd6ab44269ca5
Adrian <adrianrojasanchez@gmail.com>
2021-04-25 15:48:54 -0600

    Agregando role administrador y el menu para la administracion o gestion.

--- SISCO.Web/App_Start/FilterConfig.cs
+++ SISCO.Web/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace SISCO.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
